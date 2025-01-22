30b5c50c3d91ce17e3db33b3371c5e76ee7d6ca3
Shadow-Codes <109313532+Shadow-Codes@users.noreply.github.com>
2025-01-22 12:12:36 +0530

    Added Player class and draw player on screen

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     print("Starting asteroids!")
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -17,6 +19,7 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
